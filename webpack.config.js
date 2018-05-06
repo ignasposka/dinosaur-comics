13344c94124f22c5b475c2229b7258861467e501
Ignas <ignasp789@gmail.com>
2018-05-06 16:18:26 +0300

    Added tooling to minimize css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env']
-            }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
           }
         }
-      ]
-    }
-  };
+      },
+      {
+        test: /\.css$/,
+        use: {
+          loader: 'css-loader',
+          options: {
+            minimize: true
+          }
+        }
+      }
+    ]
+  }
+};
