beb5006f4eee4c3d641692b2eae5dedb4416fdbb
Ignas <ignasp789@gmail.com>
2018-05-28 14:08:45 +0300

    Implemented simply deploy. Added readme.

---
+++ README.md
@@ -0,0 +1,4 @@
+Steps to deploy:
+
+1.  npm run build_prod
+2.  npm run deploy
---
+++ deploy.bat
@@ -0,0 +1,2 @@
+@echo OFF
+powershell -Command "WinSCP.exe /console /script='D:\\Drive\\dinosaur_comics_generator V2\\prod_to_server.txt'"
