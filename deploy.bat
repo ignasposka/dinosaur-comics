fd367677837058353df547db1cd26fc7df707037
Ignas <ignasp789@gmail.com>
2018-05-28 16:36:27 +0300

    Implemented backup trigger before deploy.

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,3 @@
 @echo OFF
+powershell -Command "plink -ssh -i D:\Drive\Digital_Ocean_Droplet\id_rsa.ppk -noagent root@46.101.158.45 /root/scripts/make_website_backup.sh dinosaur_comics"
 powershell -Command "WinSCP.exe /console /script='D:\\Drive\\dinosaur_comics_generator V2\\prod_to_server.txt'"
