56ebcfe39343207c31e34f8328c962f50fd17218
Ignas <ignasp789@gmail.com>
2018-07-28 23:47:00 +0300

    Added lithuanian letter to the surname. Fixed encoding issue.

--- prepare-html.bat
+++ prepare-html.bat
@@ -1,3 +1,3 @@
 @echo OFF
 pause
-powershell -Command "(Get-Content prod/index.html) -replace '/dist/bundle.js', 'bundle.js' | Out-File -encoding ASCII prod/index.html"
+powershell -Command "(Get-Content -encoding utf8 prod/index.html) -replace '/dist/bundle.js', 'bundle.js' | Out-File -encoding utf8 prod/index.html"
